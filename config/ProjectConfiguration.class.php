143c1826dacf8dde31d0573480d47d755dc59e11
Sadaoui Julien <julien.sadaoui@gmail.com>
2010-11-01 18:19:01 +0100

    Ajout de Zend Framework et du plugin sfDoctrineGuardPlugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -9,5 +9,21 @@ class ProjectConfiguration extends sfProjectConfiguration
   {
     $this->enablePlugins('sfDoctrinePlugin');
     $this->enablePlugins('sfShoppingCartPlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
   }
+
+    static protected $zendLoaded = false;
+    static public function registerZend()
+    {
+        if (self::$zendLoaded)
+        {
+            return;
+        }
+        set_include_path(sfConfig::get('sf_lib_dir')
+        .'/vendor'.PATH_SEPARATOR.get_include_path());
+        require_once sfConfig::get('sf_lib_dir')
+        .'/vendor/Zend/Loader.php';
+        Zend_Loader::registerAutoload();
+        self::$zendLoaded = true;
+    }
 }
