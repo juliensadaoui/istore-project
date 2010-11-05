134bd226c7f287374e78c68f538a9324e35160c1
Sadaoui Julien <julien.sadaoui@gmail.com>
2010-11-05 10:56:37 +0100

    Ajout du menu dans le compte client

--- apps/frontend/modules/account/templates/_menu.php
+++ apps/frontend/modules/account/templates/_menu.php
@@ -1,8 +1,13 @@
 <div id="menu_customer">
     <div id="menu_account">
         <div class="menu_customer_img">
+            <a href="<?php echo url_for('@account_show'); ?>" >
+                <img src="/images/user-icon.png" alt="" width="80px" height="80px" />
+            </a>
         </div>
         <div class="menu_customer_text">
+            <span>MON COMPTE CLIENT</span><br />
+            Consulter ou mettre à jour mon adresse e-mail, mon adresse, etc.
         </div>
     </div>
 
